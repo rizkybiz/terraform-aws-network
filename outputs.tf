77db9a1cbd19e748ff6214be15f7027191c26a16
rizkybiz <justin.defrank@hashicorp.com>
2021-10-07 21:26:39 -0400

    upd: added descriptions for the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "subnet_id" {
   value = aws_subnet.demo-subnet.id
+  description = "The subnet ID to be used for an AWS instance "
 }
 
 output "vpc_security_group_id" {
   value = aws_security_group.demo-security-group.id
+  description = "The security group ID to be used for an AWS instance"
 }
